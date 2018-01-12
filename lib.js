4270d6a19ec2de3fa62fb013f8c3884afdfbe3c9
Kevin Kipp <kevin.kipp@gmail.com>
2018-01-11 21:45:13 -0600

    Add lib and build process

---
+++ lib.js
@@ -0,0 +1,10 @@
+export class Person {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+
+  greet() {
+    console.log("Hi, my name is " + this.firstname + " " + this.lastname);
+  }
+}
---
+++ rollup.config.js
@@ -0,0 +1,8 @@
+export default {
+  input: "lib.js",
+  output: {
+    file: "dist/index.js",
+    format: "umd",
+    name: "tesing_travis_ci_deploy_to_npm"
+  }
+};
