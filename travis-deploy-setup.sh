17641a795c527f5c10d18108946986becc5ad25e
Kevin Kipp <kevin.kipp@gmail.com>
2018-01-13 08:40:29 -0600

    Remove version incremenet

--- travis-deploy-setup.sh
+++ travis-deploy-setup.sh
@@ -2,7 +2,6 @@
 
 if [ -z $TRAVIS_TAG ]; then
     echo "Not a tagged release, generating commit version for package.json"
-    npm version patch
     node generateCommitVersion.js
     cat dist/package.json
 fi
