762a77f310a4a8c97b2068ae53059dd67d5d368c
Kevin Kipp <kevin.kipp@gmail.com>
2018-01-13 08:36:51 -0600

    Increment patch version

--- travis-deploy-setup.sh
+++ travis-deploy-setup.sh
@@ -2,6 +2,7 @@
 
 if [ -z $TRAVIS_TAG ]; then
     echo "Not a tagged release, generating commit version for package.json"
+    npm version patch
     node generateCommitVersion.js
     cat dist/package.json
 fi
