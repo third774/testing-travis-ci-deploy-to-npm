bf3571ffac1bebf614c83457e30ba09e52beec3b
Kevin Kipp <kevin.kipp@gmail.com>
2018-01-12 19:11:47 -0600

    Add age function

--- lib.js
+++ lib.js
@@ -1,12 +1,17 @@
 export class Person {
-  constructor(firstname, lastname) {
+  constructor(firstname, lastname, age) {
     this.firstname = firstname;
     this.lastname = lastname;
+    this.age = age;
   }
 
   greet() {
     console.log(
       "Hi, my name is " + this.firstname + " " + this.lastname + "!!!!!!!"
     );
+  }
+
+  tellAge() {
+    console.log("I'm " + this.age + " years old!");
   }
 }
